22a646cc8f3ab0bda715a66f0dded920c8b2a836
xiaoticai <xiaoticai@126.com>
2021-03-06 08:51:16 +0800

    测试打包

---
+++ XiaoTestFrame/XiaoTestFrame.framework/Headers/Person.h
@@ -0,0 +1,18 @@
+//
+//  Person.h
+//  XiaoTestFrame
+//
+//  Created by xiaoticai on 2021/3/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+/** <#annotation#> */
+@property (nonatomic, copy) NSString *name;
+- (void)eat;
+@end
+
+NS_ASSUME_NONNULL_END
